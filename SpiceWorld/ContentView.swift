f74ced2e0f1c2f686c50277232b15585b5037fa7
Jane Abernethy <jane.abernethy@hotmail.com>
2019-07-16 13:38:36 +1000

    Initial Commit

---
+++ SpiceWorld/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SpiceWorld
+//
+//  Created by Jane Abernethy on 7/16/19.
+//  Copyright © 2019 Jane Abernethy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
